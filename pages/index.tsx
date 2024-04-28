986bec6a818daa0150803736b2cac53ae14df2f5
Max Faria <127763619+max-faria@users.noreply.github.com>
2024-04-28 11:30:08 +1000

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/HomeNoAuth.module.scss'
+
+const HomeNoAuth = () => {
+  return (
+   <>
+    <Head>
+      <h1>Home</h1>
+    </Head>
+    <main></main>
+   </>
+  )
+}
+
+export default HomeNoAuth
